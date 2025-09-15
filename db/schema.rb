f132ced8d9d17fd20128225f716981a1709c6d54
hanezu1783 <spiderwort1220@gmail.com>
2025-09-15 17:32:06 +0900

    新規登録作成

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_08_194506) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
